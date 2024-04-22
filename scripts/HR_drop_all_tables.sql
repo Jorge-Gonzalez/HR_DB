6c3b41cd7fb828fe4212d91c78a04317b16b8d7d
jorge-gonzalez <jorge.gonzalez@live.com>
2024-04-21 22:58:43 -0600

    minor line order change

--- scripts/HR_drop_all_tables.sql
+++ scripts/HR_drop_all_tables.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS dependents;
+DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS countries;
